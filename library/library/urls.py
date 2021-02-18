e4b02a7268bc37d9872de8f3129175591df4b352
smagucha <smagucha@gmail.com>
2021-02-18 10:55:07 +0300

    auth

--- library/library/urls.py
+++ library/library/urls.py
@@ -14,9 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('libraryv2.urls'))
+    path('', include('libraryv2.urls')),
+    #path('accounts/', include("django.contrib.auth.urls")),
+    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    # path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    # path('accounts/password_change/',
+    #      auth_views.PasswordChangeView.as_view(template_name='accounts/password_change_form.html'),
+    #      name='password_change'),
+    # path('accounts/password_change/done/',
+    #      auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
+    #      name='password_change_done'),
+    # path('accounts/reset_password/', auth_views.PasswordResetView.as_view(template_name = "accounts/password_reset_form.html"),
+    #  name ='reset_password'),
+    # path('accounts/reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name ='accounts/password_reset_done.html'),
+    #     name='password_reset_done'),
+    # path('accounts/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name ='accounts/password_reset_confirm.html'),),
+    # path('accounts/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),),
+
 ]
